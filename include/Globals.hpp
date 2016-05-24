141112d95e7f808cc72bb86385bc4dd37bcee14e
copoutolen <thales.grilo2009@gmail.com>
2016-05-24 18:15:24 -0300

    Leitura de Config (Globals)
    Podemos agora alterar os parâmetros de Globals sem que seja necessário
    recompilar o programa.
    
    Problema: Colocar a implementação de Globals::Read no hpp causava
    definições múltiplas, portanto deixei o corpo da função na main (que é
    afinal o único lugar que chama). Será melhor criar um cpp próprio para
    Globals ou aproveitar e fundí-la a Resources, expandindo a implementação
    de Read para os nomes dos assets de jogo também?

--- include/Globals.hpp
+++ include/Globals.hpp
@@ -1,6 +1,15 @@
 #ifndef GLOBALSPANEL_HPP
 #define GLOBALSPANEL_HPP
 
+#include <iostream>
+#include <memory>
+#include <fstream>
+#include <unordered_map>
+
+//#define WINDOW_WIDTH Globals::intValues["WINDOW_WIDTH"];
+
+#define WINDOW_WIDTH_ int_values[0]
+
 class Globals
 {
 public:
@@ -13,6 +22,8 @@ public:
 
 	static float PLAYER_WALK_SPEED;
 	static float PLAYER_JUMP_SPEED;
+
+	static void Read(std::string filename_);
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,131 @@
 
 #include "../include/Game.hpp"
 #include "../include/Globals.hpp"
+#define		CONFIG_FILE	"../config.txt"
 
-int Globals::WINDOW_WIDTH = 1024;
-int Globals::WINDOW_HEIGHT = 600;
-int Globals::TILE_WIDTH = 32;
-int Globals::TILE_HEIGHT = 32;
-int Globals::GRAVITY = 200; // pixels per second
-float Globals::PLAYER_WALK_SPEED = 200; // pixels per second
-float Globals::PLAYER_JUMP_SPEED = 500; // pixels per second
+int Globals::WINDOW_WIDTH 			= 0;
+int Globals::WINDOW_HEIGHT 			= 0;
+int Globals::TILE_WIDTH 			= 0;
+int Globals::TILE_HEIGHT 			= 0;
+int Globals::GRAVITY 				= 0; // pixels per second
+float Globals::PLAYER_WALK_SPEED 	= 0; // pixels per second
+float Globals::PLAYER_JUMP_SPEED 	= 0; // pixels per second
+
+
+
 
 int main(int argc, char *argv[])
 {
-
+	Globals::Read(CONFIG_FILE);
 	Game game = Game(Globals::WINDOW_WIDTH,Globals::WINDOW_HEIGHT);
 	game.run();
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//- - - - - - - - - - - - - - - - - Globals Read Function - - - - - - - - - - - - - - - - - - - - - - - - - 
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+bool PRINTITALL = false;
+
+void Globals::Read(std::string filename_){
+	std::ifstream config;
+	config.open(filename_, std::ifstream::in);
+
+	//Tabelas Intermediárias
+	std::unordered_map<std::string, int>		intValues;
+	std::unordered_map<std::string, float>		floatValues;
+
+
+
+	//Valores Intermediários
+	int 		tempInt;
+	float 		tempFloat;
+	std::string	tempName;
+
+	if(!config.is_open())
+	{
+		std::cout << "Erro na abertura do arquivo de globals para leitura" << std::endl;
+		return;
+	}
+
+	//Aquisição de Valores
+	config >> tempName;
+	while(config.good())
+	{
+	
+		//Parsing: Tipo da Variável
+		if (tempName == "#INT")
+		{
+			config >> tempName;	//Nome da primeira variável
+			while (tempName[0] != '#')
+			{
+				config >> tempInt;		//Valor Inteiro
+				intValues.emplace(tempName, tempInt);
+				if (PRINTITALL) std::cout << tempName << " = " << tempInt << std::endl;
+
+				config >> tempName;		//Nome da próxima variável
+			}
+		}
+
+		std::cout << tempName << std::endl;
+		//Parsing: Tipo da Variável
+		if (tempName == "#FLOAT")
+		{
+			config >> tempName;	//Nome da primeira variável
+			while (tempName[0] != '#')
+			{
+				config >> tempFloat;		//Valor Inteiro
+				if (PRINTITALL) std::cout << tempName << " = " << tempFloat << std::endl;
+				floatValues.emplace(tempName, tempFloat);
+
+				config >> tempName;			//Nome da próxima variável
+			}
+		}
+		
+		config >> tempName;
+	}
+
+	config.close();
+
+	//Assossiação de Valores
+	WINDOW_WIDTH 		= intValues["WINDOW_WIDTH"];
+	WINDOW_HEIGHT 		= intValues["WINDOW_HEIGHT"];
+	TILE_WIDTH 			= intValues["TILE_WIDTH"];
+	TILE_HEIGHT 		= intValues["TILE_HEIGHT"];
+	GRAVITY 			= intValues["GRAVITY"];
+
+	PLAYER_JUMP_SPEED 	= floatValues["PLAYER_JUMP_SPEED"];
+	PLAYER_WALK_SPEED 	= floatValues["PLAYER_WALK_SPEED"];
+	
+
+
+	//Test Printing
+	if (true){
+		std::cout << "Value WINDOW_WIDTH now equals: " 	<< intValues["WINDOW_WIDTH"] << std::endl;
+		std::cout << "Value WINDOW_HEIGHT now equals: " << intValues["WINDOW_HEIGHT"] << std::endl;
+		std::cout << "Value TILE_WIDTH now equals: " 	<< intValues["TILE_WIDTH"] << std::endl;
+		std::cout << "Value TILE_HEIGHT now equals: " 	<< intValues["TILE_HEIGHT"] << std::endl;
+		std::cout << "Value GRAVITY now equals: " 		<< intValues["GRAVITY"] << std::endl << std::endl;
+
+		std::cout << "Value PLAYER_JUMP_SPEED now equals: " << floatValues["PLAYER_JUMP_SPEED"] << std::endl;
+		std::cout << "Value PLAYER_WALK_SPEED now equals: " << floatValues["PLAYER_WALK_SPEED"] << std::endl;
+	}
+
+	intValues.clear();
+	floatValues.clear();
+}
+
