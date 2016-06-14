0b7241c5951edd513dae416795934200421f6c1c
victormefer <victormefer@gmail.com>
2016-06-14 15:09:06 -0300

    Tirando arquivos inuteis

--- include/StateComponent.hpp
+++ include/StateComponent.hpp
@@ -3,37 +3,6 @@
 
 #include "Component.hpp"
 
-/*class EntityStateMachine;
-class EntityState;
-
-enum StateMachineType
-{
-	PLAYER,
-	ENEMY,
-	OBJECT
-};
-
-class StateComponent : public Component
-{
-public:
-	StateComponent(StateMachineType type = PLAYER);
-	~StateComponent() {}
-
-	EntityStateMachine* getStateMachine() { return stateMachine_; }
-	void setStateMachine(EntityStateMachine& stateMachine, StateMachineType type = PLAYER) { stateMachine_ = &stateMachine; }
-	EntityState* getState();
-
-	bool isFacingRight() { return facingRight_; }
-	void setFacingRight(bool facingRight) { facingRight_ = facingRight; }
-
-private:
-	EntityStateMachine* stateMachine_;
-	bool facingRight_;
-};
-
-#include "EntityStateMachine.hpp"
-#include "EntityState.hpp"*/
-
 enum State
 {
 	IDLE,
