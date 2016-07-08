8acbf5e2eea26e8b7b3ca9a53c3b16d818495f89
victormefer <victormefer@gmail.com>
2016-07-08 04:28:40 -0300

    coisas

--- include/HealthSystem.hpp
+++ include/HealthSystem.hpp
@@ -5,12 +5,14 @@
 
 #include "System.hpp"
 #include "GameState.hpp"
+#include "Timer.hpp"
 
 class HealthSystem : public System
 {
 public:
 	HealthSystem();
 	void update(float dt, GameState& gameState);
+	Timer playerRecoverTime_;
 };
 
 #endif
